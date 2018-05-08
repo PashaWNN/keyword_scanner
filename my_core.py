d6842312314a71ab1dfa0cfcfe31f65142caeb0b
Pavel Shishmarev <pashawnn@gmail.com>
2018-05-08 18:19:37 +0900

    fix

--- my_core.py
+++ my_core.py
@@ -93,11 +93,11 @@ def strip_html(h):
     Обрезанный текст
   """
   reg = r'(?:description" content="(.+)"|<title>(.+)<\/title>|<body>([\S\s]+)<\/body>)'
-  res = re.findall(reg, h, flags=re.IGNORECASE)
+  res = '\n\n'.join(re.findall(reg, h, flags=re.IGNORECASE)[0])
   br = '(?:%s)' % '|'.join(blacklist)
   res = re.sub(br, '', res)
   if len(res)>0:
-    return '\n\n'.join(res[0])
+    return res
 
 
 def count_occurences(string, page):
